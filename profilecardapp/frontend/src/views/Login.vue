5b7b72b8a357b9656758f5b72470197bde166113
LRCozier <lrcozier@gmail.com>
2025-10-02 12:01:26 +0100

    created login and register views

---
+++ profilecardapp/frontend/src/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+  <LoginRegister :is-login="true" />
+</template>
+
+<script setup>
+import LoginRegister from '../components/LoginRegister.vue'
+</script>
---
+++ profilecardapp/frontend/src/views/Register.vue
@@ -0,0 +1,7 @@
+<template>
+  <LoginRegister :is-login="false" />
+</template>
+
+<script setup>
+import LoginRegister from '../components/LoginRegister.vue'
+</script>
